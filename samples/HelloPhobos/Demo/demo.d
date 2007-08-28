69f0cfbaba4e27fb252c32ff24b12580e034a4e0
flithm <unknown>
2007-08-28 11:37:16 +0000

    Add Phobos sample

---
+++ samples/HelloPhobos/Demo/demo.d
@@ -0,0 +1,9 @@
+import Hello.hello;
+
+int main()
+{
+  Hail hello = new Hail();
+  hello.Print();
+  
+  return 0;
+}
---
+++ samples/HelloPhobos/Hello/hello.d
@@ -0,0 +1,14 @@
+module Hello.hello;
+
+import std.stdio;
+
+class Hail
+{
+public:
+  void Print()
+  {
+     writefln ("Hello, World!");
+  }
+}
+
+
