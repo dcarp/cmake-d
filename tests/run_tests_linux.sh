f65250a4e2b24670afb94a8d3a5ec12a0d4905ab
Dragos Carp <dragoscarp@gmail.com>
2016-08-06 18:21:07 +0200

    Fix #11: DMD: enable MinSizeRel with -L-s

--- tests/run_tests_linux.sh
+++ tests/run_tests_linux.sh
@@ -10,17 +10,43 @@ TEST_DIR=../../cmake-d-test-build
 rm -rf $TEST_DIR
 mkdir -p $TEST_DIR
 cd $TEST_DIR
-mkdir -p release
-cd release
-# Do a release build
+
+# Do a unspecified build
+mkdir -p unspecified
+pushd unspecified
 cmake -DCMAKE_MODULE_PATH:PATH=$CMAKE_DIR ../../cmake-d/tests
 make
 make test
-cd ..
+popd
 
 # Do a debug build
 mkdir -p debug
-cd debug
-cmake -DCMAKE_MODULE_PATH:PATH=$CMAKE_DIR -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=debug ../../cmake-d/tests
+pushd debug
+cmake -DCMAKE_MODULE_PATH:PATH=$CMAKE_DIR -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Debug ../../cmake-d/tests
 make
 make test
+popd
+
+# Do a release build
+mkdir -p release
+pushd release
+cmake -DCMAKE_MODULE_PATH:PATH=$CMAKE_DIR -DCMAKE_BUILD_TYPE=Release ../../cmake-d/tests
+make
+make test
+popd
+
+# Do a relWithDebInfo build
+mkdir -p relWithDebInfo
+pushd relWithDebInfo
+cmake -DCMAKE_MODULE_PATH:PATH=$CMAKE_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo ../../cmake-d/tests
+make
+make test
+popd
+
+# Do a minSizeRel build
+mkdir -p minSizeRel
+pushd minSizeRel
+cmake -DCMAKE_MODULE_PATH:PATH=$CMAKE_DIR -DCMAKE_BUILD_TYPE=MinSizeRel ../../cmake-d/tests
+make
+make test
+popd
